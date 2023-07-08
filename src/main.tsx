524cdfc7429f057f6f22cdbee71d8953f769df14
kangod <kangod.yan@gmail.com>
2023-07-08 13:51:15 +0800

    1、引入react-router-dom，使用BrowserRouter方式 2、新增可配置式路由，添加首页和404页面

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
 import { ApolloProvider } from '@apollo/client';
 import ReactDOM from 'react-dom/client';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
 import { client } from './utils/apollo';
-import App from './App';
 import './index.css';
+import { ROUTE_CONFIG } from './routes';
+import Page404 from './containers/Page404';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ApolloProvider client={client}>
-    <App />
+    {/* 浏览器导航 */}
+    <BrowserRouter>
+      <Routes>
+        {/* 循环路由配置 */}
+        {ROUTE_CONFIG.map((item) => (
+          <Route
+            key={item.key}
+            path={item.path}
+            element={<item.element />}
+          />
+        ))}
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </BrowserRouter>
   </ApolloProvider>,
 );
